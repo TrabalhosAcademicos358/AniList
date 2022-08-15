71e3f89bb5ef916c3482e1a8e41d55a219971ff2
Pedro <pedrojuniorifpb@gmail.com>
2022-08-15 11:19:47 -0300

    Adicionando os setters na interface Midia

--- src/Interfaces/Midia.java
+++ src/Interfaces/Midia.java
@@ -2,7 +2,10 @@ package Interfaces;
 
 public interface Midia {
     double getNota();
+    void setTitulo(String titulo);
     String getTitulo();
+    void setGeneros(String[] generos);
     String[] getGeneros();
+    void setAutor(String autor);
     String getAutor();
 }
